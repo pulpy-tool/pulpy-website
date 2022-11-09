73551e2425063d3a933efa791d548f104c2e7d9a
gnagaraj7793 <gnagaraj7793@gmail.com>
2022-11-09 22:09:20 +0530

    Loading Fixes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,26 +5,31 @@ import 'rc-drawer/assets/index.css';
 import 'rc-tabs/assets/index.css';
 import 'swiper/swiper-bundle.css';
 import 'assets/css/styles.css';
-import Loader from 'components/loader/loader';
+import NProgress from 'nprogress'; //nprogress module
+import 'nprogress/nprogress.css'; //styles of nprogress
+
+// import Loader from 'components/loader/loader';
 
 export default function CustomApp({ Component, pageProps }) {
-  const [loading, setLoading] = React.useState(false);
+  // const [loading, setLoading] = React.useState(false);
   useEffect(() => {
     initGA();
     logPageView();
-    setTimeout(() => setLoading(true), 3000);
-    Router.events.on('routeChangeComplete', logPageView);
+    // setTimeout(() => setLoading(true), 3000);
+    Router.events.on('routeChangeStart', () => NProgress.start()); Router.events.on('routeChangeComplete', () => NProgress.done()); Router.events.on('routeChangeError', () => NProgress.done());  
+    
   }, []);
 
   return (
     <>    
-      {!loading ? (
+      {/* {!loading ? (
         <React.Fragment>
-            <Component {...pageProps} />
+           
         </React.Fragment>
       ) :(
         <Loader/>
-      )}
+      )} */}
+       <Component {...pageProps} />
     
     </>
   );
